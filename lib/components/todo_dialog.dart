1ad860465b4efb005c6c95904456477ae2419885
stark <mrcs.stark@gmail.com>
2019-02-25 22:30:40 -0300

    Estilizando dialog

--- lib/components/todo_dialog.dart
+++ lib/components/todo_dialog.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
 
 class TodoDialog extends StatelessWidget {
-
   final controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Nova Tarefa'),
-      content: TextField(
-        controller: controller,
-        autofocus: true,
+    return Theme(
+      data: Theme.of(context)
+          .copyWith(dialogBackgroundColor: Color.fromRGBO(58, 66, 86, 1.0)),
+      child: AlertDialog(
+        title: Text(
+          'Nova Tarefa',
+          style: TextStyle(color: Colors.white),
+        ),
+        content: TextField(
+          cursorColor: Colors.white,
+          controller: controller,
+          autofocus: true,
+          decoration: InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.cyan),
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Cancelar', style: TextStyle(color: Colors.cyanAccent)),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          FlatButton(
+            child: Text('Add', style: TextStyle(color: Colors.cyanAccent)),
+            onPressed: () {
+              final todo = controller.value.text;
+              controller.clear();
+              Navigator.of(context).pop(todo);
+            },
+          ),
+        ],
       ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('Cancelar'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        FlatButton(
-          child: Text('Add'),
-          onPressed: () {
-            final todo = controller.value.text;
-            controller.clear();
-            Navigator.of(context).pop(todo);
-          },
-        ),
-      ],
     );
   }
 }
-
